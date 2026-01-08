8fabd0c00be3c0e60c0b70bf135c2e11634ca425
telly3e <51300254+telly3e@users.noreply.github.com>
2026-01-08 22:34:29 +0000

    update src file from pakeplus

---
+++ scripts/www/assets/chunk-ZFYBQKZZ.js
@@ -0,0 +1,47 @@
+import {
+  AbstractMermaidTokenBuilder,
+  CommonValueConverter,
+  EmptyFileSystem,
+  GitGraphGeneratedModule,
+  MermaidGeneratedSharedModule,
+  __name,
+  createDefaultCoreModule,
+  createDefaultSharedCoreModule,
+  inject,
+  lib_exports
+} from "./chunk-OBZA3MUO.js";
+
+// ../../node_modules/@mermaid-js/parser/dist/chunks/mermaid-parser.core/chunk-S6J4BHB3.mjs
+var GitGraphTokenBuilder = class extends AbstractMermaidTokenBuilder {
+  static {
+    __name(this, "GitGraphTokenBuilder");
+  }
+  constructor() {
+    super(["gitGraph"]);
+  }
+};
+var GitGraphModule = {
+  parser: {
+    TokenBuilder: /* @__PURE__ */ __name(() => new GitGraphTokenBuilder(), "TokenBuilder"),
+    ValueConverter: /* @__PURE__ */ __name(() => new CommonValueConverter(), "ValueConverter")
+  }
+};
+function createGitGraphServices(context = EmptyFileSystem) {
+  const shared = inject(
+    createDefaultSharedCoreModule(context),
+    MermaidGeneratedSharedModule
+  );
+  const GitGraph = inject(
+    createDefaultCoreModule({ shared }),
+    GitGraphGeneratedModule,
+    GitGraphModule
+  );
+  shared.ServiceRegistry.register(GitGraph);
+  return { shared, GitGraph };
+}
+__name(createGitGraphServices, "createGitGraphServices");
+
+export {
+  GitGraphModule,
+  createGitGraphServices
+};
