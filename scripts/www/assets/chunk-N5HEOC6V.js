43f9e6dd92203359b0f148be1920e6c7efb4890b
telly3e <51300254+telly3e@users.noreply.github.com>
2026-01-08 22:34:14 +0000

    update src file from pakeplus

---
+++ scripts/www/assets/chunk-N5HEOC6V.js
@@ -0,0 +1,47 @@
+import {
+  AbstractMermaidTokenBuilder,
+  CommonValueConverter,
+  EmptyFileSystem,
+  InfoGeneratedModule,
+  MermaidGeneratedSharedModule,
+  __name,
+  createDefaultCoreModule,
+  createDefaultSharedCoreModule,
+  inject,
+  lib_exports
+} from "./chunk-OBZA3MUO.js";
+
+// ../../node_modules/@mermaid-js/parser/dist/chunks/mermaid-parser.core/chunk-LBM3YZW2.mjs
+var InfoTokenBuilder = class extends AbstractMermaidTokenBuilder {
+  static {
+    __name(this, "InfoTokenBuilder");
+  }
+  constructor() {
+    super(["info", "showInfo"]);
+  }
+};
+var InfoModule = {
+  parser: {
+    TokenBuilder: /* @__PURE__ */ __name(() => new InfoTokenBuilder(), "TokenBuilder"),
+    ValueConverter: /* @__PURE__ */ __name(() => new CommonValueConverter(), "ValueConverter")
+  }
+};
+function createInfoServices(context = EmptyFileSystem) {
+  const shared = inject(
+    createDefaultSharedCoreModule(context),
+    MermaidGeneratedSharedModule
+  );
+  const Info = inject(
+    createDefaultCoreModule({ shared }),
+    InfoGeneratedModule,
+    InfoModule
+  );
+  shared.ServiceRegistry.register(Info);
+  return { shared, Info };
+}
+__name(createInfoServices, "createInfoServices");
+
+export {
+  InfoModule,
+  createInfoServices
+};
