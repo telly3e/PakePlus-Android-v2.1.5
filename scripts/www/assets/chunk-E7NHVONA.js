0d4603d883b638b3517854c92e0fe30a9d47235e
telly3e <51300254+telly3e@users.noreply.github.com>
2026-01-08 22:34:04 +0000

    update src file from pakeplus

---
+++ scripts/www/assets/chunk-E7NHVONA.js
@@ -0,0 +1,74 @@
+import {
+  __name
+} from "./chunk-OBZA3MUO.js";
+
+// ../../node_modules/@mermaid-js/parser/dist/mermaid-parser.core.mjs
+var parsers = {};
+var initializers = {
+  info: /* @__PURE__ */ __name(async () => {
+    const { createInfoServices: createInfoServices2 } = await import("./info-NVLQJR56-GDSGBQLH.js");
+    const parser = createInfoServices2().Info.parser.LangiumParser;
+    parsers.info = parser;
+  }, "info"),
+  packet: /* @__PURE__ */ __name(async () => {
+    const { createPacketServices: createPacketServices2 } = await import("./packet-BFZMPI3H-LHFCH3XT.js");
+    const parser = createPacketServices2().Packet.parser.LangiumParser;
+    parsers.packet = parser;
+  }, "packet"),
+  pie: /* @__PURE__ */ __name(async () => {
+    const { createPieServices: createPieServices2 } = await import("./pie-7BOR55EZ-KOZCQATS.js");
+    const parser = createPieServices2().Pie.parser.LangiumParser;
+    parsers.pie = parser;
+  }, "pie"),
+  architecture: /* @__PURE__ */ __name(async () => {
+    const { createArchitectureServices: createArchitectureServices2 } = await import("./architecture-U656AL7Q-UMXUWOJN.js");
+    const parser = createArchitectureServices2().Architecture.parser.LangiumParser;
+    parsers.architecture = parser;
+  }, "architecture"),
+  gitGraph: /* @__PURE__ */ __name(async () => {
+    const { createGitGraphServices: createGitGraphServices2 } = await import("./gitGraph-F6HP7TQM-HLWFOJFB.js");
+    const parser = createGitGraphServices2().GitGraph.parser.LangiumParser;
+    parsers.gitGraph = parser;
+  }, "gitGraph"),
+  radar: /* @__PURE__ */ __name(async () => {
+    const { createRadarServices: createRadarServices2 } = await import("./radar-NHE76QYJ-TW2375CX.js");
+    const parser = createRadarServices2().Radar.parser.LangiumParser;
+    parsers.radar = parser;
+  }, "radar"),
+  treemap: /* @__PURE__ */ __name(async () => {
+    const { createTreemapServices: createTreemapServices2 } = await import("./treemap-KMMF4GRG-5Z6HN2V3.js");
+    const parser = createTreemapServices2().Treemap.parser.LangiumParser;
+    parsers.treemap = parser;
+  }, "treemap")
+};
+async function parse(diagramType, text) {
+  const initializer = initializers[diagramType];
+  if (!initializer) {
+    throw new Error(`Unknown diagram type: ${diagramType}`);
+  }
+  if (!parsers[diagramType]) {
+    await initializer();
+  }
+  const parser = parsers[diagramType];
+  const result = parser.parse(text);
+  if (result.lexerErrors.length > 0 || result.parserErrors.length > 0) {
+    throw new MermaidParseError(result);
+  }
+  return result.value;
+}
+__name(parse, "parse");
+var MermaidParseError = class extends Error {
+  constructor(result) {
+    const lexerErrors = result.lexerErrors.map((err) => err.message).join("\n");
+    const parserErrors = result.parserErrors.map((err) => err.message).join("\n");
+    super(`Parsing failed: ${lexerErrors} ${parserErrors}`);
+    this.result = result;
+  }
+  static {
+    __name(this, "MermaidParseError");
+  }
+};
+
+export {
+  parse
+};
