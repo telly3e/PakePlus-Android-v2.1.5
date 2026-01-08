4e2cbae5a8d34fcfd495e3ac558574eb957b3728
telly3e <51300254+telly3e@users.noreply.github.com>
2026-01-08 22:34:31 +0000

    update src file from pakeplus

---
+++ scripts/www/assets/classDiagram-2ON5EDUG-TIP4LGG2.js
@@ -0,0 +1,43 @@
+import {
+  ClassDB,
+  classDiagram_default,
+  classRenderer_v3_unified_default,
+  styles_default
+} from "./chunk-RQP5TY6Y.js";
+import "./chunk-K6AV3VJK.js";
+import "./chunk-PFXQX2Y7.js";
+import "./chunk-6SIZP672.js";
+import "./chunk-WINAANFJ.js";
+import "./chunk-H5PYWCUD.js";
+import "./chunk-3X24VTB7.js";
+import "./chunk-ALQLWK4Q.js";
+import "./chunk-IK2YVULW.js";
+import "./chunk-T6N5NIZF.js";
+import "./chunk-ERXSOLO3.js";
+import "./chunk-F2OBZR3M.js";
+import "./chunk-7IK5N4XB.js";
+import "./chunk-OIESMJRM.js";
+import "./chunk-AC74IHDH.js";
+import {
+  __name
+} from "./chunk-2DQTVN24.js";
+import "./chunk-2P3A4VVY.js";
+
+// ../../node_modules/mermaid/dist/chunks/mermaid.core/classDiagram-2ON5EDUG.mjs
+var diagram = {
+  parser: classDiagram_default,
+  get db() {
+    return new ClassDB();
+  },
+  renderer: classRenderer_v3_unified_default,
+  styles: styles_default,
+  init: /* @__PURE__ */ __name((cnf) => {
+    if (!cnf.class) {
+      cnf.class = {};
+    }
+    cnf.class.arrowMarkerAbsolute = cnf.arrowMarkerAbsolute;
+  }, "init")
+};
+export {
+  diagram
+};
