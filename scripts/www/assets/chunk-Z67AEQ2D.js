9dc211c335a600817464077467761c6419ce92c6
telly3e <51300254+telly3e@users.noreply.github.com>
2026-01-08 22:34:28 +0000

    update src file from pakeplus

---
+++ scripts/www/assets/chunk-Z67AEQ2D.js
@@ -0,0 +1,47 @@
+import {
+  AbstractMermaidTokenBuilder,
+  CommonValueConverter,
+  EmptyFileSystem,
+  MermaidGeneratedSharedModule,
+  RadarGeneratedModule,
+  __name,
+  createDefaultCoreModule,
+  createDefaultSharedCoreModule,
+  inject,
+  lib_exports
+} from "./chunk-OBZA3MUO.js";
+
+// ../../node_modules/@mermaid-js/parser/dist/chunks/mermaid-parser.core/chunk-LHMN2FUI.mjs
+var RadarTokenBuilder = class extends AbstractMermaidTokenBuilder {
+  static {
+    __name(this, "RadarTokenBuilder");
+  }
+  constructor() {
+    super(["radar-beta"]);
+  }
+};
+var RadarModule = {
+  parser: {
+    TokenBuilder: /* @__PURE__ */ __name(() => new RadarTokenBuilder(), "TokenBuilder"),
+    ValueConverter: /* @__PURE__ */ __name(() => new CommonValueConverter(), "ValueConverter")
+  }
+};
+function createRadarServices(context = EmptyFileSystem) {
+  const shared = inject(
+    createDefaultSharedCoreModule(context),
+    MermaidGeneratedSharedModule
+  );
+  const Radar = inject(
+    createDefaultCoreModule({ shared }),
+    RadarGeneratedModule,
+    RadarModule
+  );
+  shared.ServiceRegistry.register(Radar);
+  return { shared, Radar };
+}
+__name(createRadarServices, "createRadarServices");
+
+export {
+  RadarModule,
+  createRadarServices
+};
